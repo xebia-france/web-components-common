a1471140b1660eb777c87da22f45fa25760ba5ec
paque <paque.melanie@gmail.com>
2020-02-04 12:53:19 +0100

    debug nav 4

--- src/components/NavigationBasic/NavigationBasic.js
+++ src/components/NavigationBasic/NavigationBasic.js
@@ -47,7 +47,7 @@ class NavigationBasic extends Component {
             }
         }*/
 
-        if (localStorage.getItem('reloadLanguage')) {
+        /*if (localStorage.getItem('reloadLanguage')) {
             const reloadLanguage = localStorage.getItem('reloadLanguage');
             if (reloadLanguage === 'true') {
                 this.setState({
@@ -56,7 +56,7 @@ class NavigationBasic extends Component {
                     localStorage.setItem('reloadLanguage', 'false');
                 })
             }
-        }
+        }*/
     }
 
     componentDidUpdate(prevProps) {
@@ -205,7 +205,7 @@ class NavigationBasic extends Component {
                 >
                     <Top>
                         <div><a href={`/${locale.split('-')[0]}`} onClick={() => {
-                            localStorage.setItem('scrollPosition', 0);
+                           // localStorage.setItem('scrollPosition', 0);
                         }}>
                             {
                                 fields['Image'] ?
@@ -262,8 +262,8 @@ class NavigationBasic extends Component {
                                                      className={l === locale ? 'selected' : ''}
                                                      href={this.getUrlWithLocale(l, location.pathname)}
                                                      onClick={() => {
-                                                         localStorage.setItem('scrollPosition', this.state.scrollY);
-                                                         localStorage.setItem('reloadLanguage', 'true');
+                                                         //localStorage.setItem('scrollPosition', this.state.scrollY);
+                                                         //localStorage.setItem('reloadLanguage', 'true');
                                                      }}
                                         >
                                             {l.split('-')[0]}
