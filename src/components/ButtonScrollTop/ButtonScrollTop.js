a22cf1776cc3032a3a1401c0dd52774a60c6e0ab
paque <paque.melanie@gmail.com>
2020-02-06 15:56:22 +0100

    behavior scroll

--- src/components/ButtonScrollTop/ButtonScrollTop.js
+++ src/components/ButtonScrollTop/ButtonScrollTop.js
@@ -63,7 +63,7 @@ class ButtonScrollTop extends Component {
     scrollToTop = () => {
         if (typeof window !== 'undefined') {
             window.scroll({top: 0, left: 0, behavior: 'smooth'});
-            localStorage.resetItem('scrollPosition');
+            localStorage.removeItem('scrollPosition');
         }
     }
 
