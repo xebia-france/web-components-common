3817880f2c7280cfdd533266524565887950e223
paque <paque.melanie@gmail.com>
2020-10-07 17:16:12 +0200

    update Cardiframe

--- src/components/CardIframe/styled.js
+++ src/components/CardIframe/styled.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 import {ContainerCommon ,ContentCommon, TextCommon, ImageContainerCommon, CTACommon } from '../../styles/common.styled';
+import { device} from "../../styles/constants";
+import {isNumber} from "../../utils/functions";
+import {generateSize} from "../../utils/StyleGenerator";
 
 export const Container = styled(ContainerCommon)``;
 
@@ -11,11 +14,26 @@ export const ImageContainer = styled(ImageContainerCommon)``;
 
 export const CTA = styled(CTACommon)``;
 
-export const Iframe = styled.div`
+export const Iframe = styled.div.attrs(props => ({
+    responsive: props.responsive,
+    basis: props.basis
+}))`
     width : 100%;
     height : auto;
     z-index : 10;
     display : flex;
     flex-direction: column;
     align-items: center;
+    
+    ${ props => props.responsive.map((size, i) => `
+         @media ${ device[size] } {
+                ${ props.basis ? `
+                    & iframe {
+                        ${ props.basis ? generateSize(props.basis, size) : '' }
+                    }
+                
+                `       
+              : ''}
+         }`)
+    };
 `
