36a216167344d87dde2c715405c6d5fc5bba410f
paque <paque.melanie@gmail.com>
2020-12-08 17:30:41 +0100

    update ListFormationsSmallCard

--- src/sections/ListFormationsSmallCard/ListFormationsSmallCard.js
+++ src/sections/ListFormationsSmallCard/ListFormationsSmallCard.js
@@ -39,58 +39,6 @@ const ListFormationsSmallCard = ({children, fields, name, assetsDirectory, data,
     );
 };
 
-ListFormationsSmallCard.defaultProps = {
-    fields: {
-        Template: {
-            content: {},
-            responsiveSettings: ['A'],
-            settings: {
-                basis: {
-                    A: {
-                        padding: {
-                            top: '0',
-                            right: '0',
-                            bottom: '0',
-                            left: '0'
-                        },
-                        color: {
-                            hex: '#000000',
-                            rgb: '0,0,0',
-                            name: 'black',
-                            shade: null
-                        },
-                        opacity: {
-                            value: '1'
-                        }
-                    }
-                }
-
-            }
-        },
-        FlexContainer: {
-            content: {},
-            responsiveSettings: ['A'],
-            settings: {
-                flex: {
-                    A: {
-                        properties: {
-                            columns: '1',
-                            gutterHorizontal: '0',
-                            gutterVertical: '0',
-                            direction: 'row',
-                            wrap: 'wrap',
-                            justify: 'flex-start',
-                            alignItems: 'flex-start',
-                            alignContent: 'flex-start'
-                        }
-                    }
-                }
-            }
-
-
-        }
-
-    }
-}
+ListFormationsSmallCard.defaultProps = {}
 
 export default ListFormationsSmallCard;
