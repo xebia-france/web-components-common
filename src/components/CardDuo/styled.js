f5c8ba35b80bb4551cb92b877e66ccf28c96e972
paque <paque.melanie@gmail.com>
2021-03-03 16:27:40 +0100

    update Card Duo

--- src/components/CardDuo/styled.js
+++ src/components/CardDuo/styled.js
@@ -14,6 +14,8 @@ export const Container = styled.div``;
 
 export const SubContainer = styled.div`
    z-index : 2;
+   width : 100%;
+   height : 100%;
 `;
 export const PrevContainer = styled.div`
     display : flex;
