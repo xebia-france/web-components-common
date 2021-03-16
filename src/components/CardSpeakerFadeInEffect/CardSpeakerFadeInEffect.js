6f4012c7abb788854e2b2270bee36770326b1f9d
paque <paque.melanie@gmail.com>
2021-03-16 14:29:01 +0100

    debug SSR 2

--- src/components/CardSpeakerFadeInEffect/CardSpeakerFadeInEffect.js
+++ src/components/CardSpeakerFadeInEffect/CardSpeakerFadeInEffect.js
@@ -1,38 +1,11 @@
-import React, { useRef, useLayoutEffect, useState } from 'react';
-import {Container,ContainerActive, ShortPresentation} from './styled';
+import React, {useRef, useLayoutEffect, useState} from 'react';
+import {Container, ContainerActive, ShortPresentation} from './styled';
 import Text from '../../functional/Text';
 import Content from '../../functional/Content';
-import CTA from '../../functional/CTA'
-import Image from "../../functional/Image";
-import { TextCommon} from "../../styles/common.styled";
+import {TextCommon} from "../../styles/common.styled";
 
 import PropTypes from 'prop-types';
 import {getContentProps, getTemplatePropsWithImage, getTextProps} from "../../utils/gettersProperties";
-import {useWindowSize} from "../../utils/customHooks";
-
-
-const buildComponent = (fields, field, language, assetsDirectory, key) => {
-    if (!fields[field]) return
-    switch (field) {
-        case 'Title':
-            return <Text key={key} field={fields[field]} language={language}/>;
-
-        case 'Tagline':
-            return <Text key={key} field={fields[field]} language={language}/>;
-
-        case 'Content':
-            return <Content key={key} field={fields[field]} language={language}/>;
-
-        case 'Image':
-            return <Image key={key} field={fields[field]} language={language}
-                          assetsDirectory={assetsDirectory}/>
-
-        case 'CTA':
-            return <CTA key={key} field={fields[field]} language={language}/>;
-        default :
-            return null;
-    }
-}
 
 const CardSpeakerFadeInEffect = ({fields, order, assetsDirectory, language}) => {
     const ContentBold = fields.ContentBold ? {...getContentProps(fields.ContentBold)} : null;
@@ -41,20 +14,13 @@ const CardSpeakerFadeInEffect = ({fields, order, assetsDirectory, language}) => 
     const [isActive, setIsActive] = useState(false);
 
     const targetRef = useRef();
-    const [dimensions, setDimensions] = useState({ width:0, height: 0 });
+    const [dimensions, setDimensions] = useState({width: 0, height: 0});
 
-    // holds the timer for setTimeout and clearInterval
     let movement_timer = null;
-
-    // the number of ms the window size must stay the same size before the
-    // dimension state variable is reset
     const RESET_TIMEOUT = 100;
 
     const test_dimensions = () => {
-        // For some reason targetRef.current.getBoundingClientRect was not available
-        // I found this worked for me, but unfortunately I can't find the
-        // documentation to explain this experience
-        if (targetRef.current) {
+       if (targetRef.current) {
             setDimensions({
                 width: targetRef.current.offsetWidth,
                 height: targetRef.current.offsetHeight
@@ -63,21 +29,22 @@ const CardSpeakerFadeInEffect = ({fields, order, assetsDirectory, language}) => 
         }
     }
 
-    // This sets the dimensions on the first render
     useLayoutEffect(() => {
         test_dimensions();
     }, []);
 
     if (typeof window !== 'undefined' && typeof document !== `undefined`) {
-        window.addEventListener('resize', ()=>{
+        window.addEventListener('resize', () => {
             clearInterval(movement_timer);
             movement_timer = setTimeout(test_dimensions, RESET_TIMEOUT);
         });
     }
 
     return (
-        <Container ref={targetRef} dynamicHeight={dimensions.width} fadeAnimation={fields.TemplateActive}  ref={targetRef}  {...getTemplatePropsWithImage(fields.Template)} contentBold={ContentBold}
-                    assetsDirectory={assetsDirectory} className={isActive ? 'active' : ''} onClick={() => setIsActive(!isActive)}>
+        <Container dynamicHeight={dimensions.width} fadeAnimation={fields.TemplateActive}
+                   ref={targetRef}  {...getTemplatePropsWithImage(fields.Template)} contentBold={ContentBold}
+                   assetsDirectory={assetsDirectory} className={isActive ? 'active' : ''}
+                   onClick={() => setIsActive(!isActive)}>
             <ShortPresentation>
                 {fields.TextName && <Text field={fields.TextName} language={language}/>}
                 {fields.TextJob && <Text field={fields.TextJob} language={language}/>}
@@ -85,9 +52,9 @@ const CardSpeakerFadeInEffect = ({fields, order, assetsDirectory, language}) => 
             {
                 fields.TemplateActive &&
                 <ContainerActive  {...getTemplatePropsWithImage(fields.TemplateActive)} contentBold={ContentBold}
-                                                       assetsDirectory={assetsDirectory}>
+                                  assetsDirectory={assetsDirectory}>
                     {fields.TextNameActive &&
-                        <TextCommon {...getTextProps(fields.TextNameActive)}>{fields.TextName.content.text ? fields.TextName.content.text[language] : null}</TextCommon>
+                    <TextCommon {...getTextProps(fields.TextNameActive)}>{fields.TextName.content.text ? fields.TextName.content.text[language] : null}</TextCommon>
                     }
                     {fields.ContentTalksSession && <Content field={fields.ContentTalksSession} language={language}/>}
                     {fields.ContentTalksInfo && <Content field={fields.ContentTalksInfo} language={language}/>}
